2c12bd55273edc61d669b006ed94c56d2585a34b
pereza <aperez@predictia.es>
2021-05-17 09:51:17 +0200

    Repository initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bias correction of air quality CAMS model predictions by using OpenAQ observations.',
+    author='Antonio Pérez Velasco / Mario Santa Cruz López',
+    license='MIT',
+)
