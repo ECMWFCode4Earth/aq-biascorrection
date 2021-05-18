1868d04ed6751d86a2df9af2867b255909ea36c1
santacruzm <Mario_1998>
2021-05-18 13:25:13 +0200

    Unifying code style.

--- src/data/downloader.py
+++ src/data/downloader.py
@@ -1,30 +1,29 @@
-import os
-import openaq
 from pathlib import Path
-import pandas as pd
-import numpy as np
+from dataclasses import dataclass
+from typing import Dict
 from math import sin, cos, sqrt, atan2, radians
 
+import pandas as pd
+import numpy as np
+import os
+import openaq
 
+
+tol = 1e-3
+
+
+@dataclass
 class Location:
     """
     Class to define specific location of interest
     with its correspondent attributes
     """
-    def __init__(
-            self,
-            location_id: str,
-            city: str,
-            country: str,
-            latitude: float,
-            longitude: float,
-    ):
-        self.location_id = location_id
-        self.latitude = round(latitude, 2)
-        self.longitude = round(longitude, 2)
-        self.city = city
-        self.country = country
-
+    location_id: str
+    city: str
+    country: str
+    latitude: float
+    longitude: float
+    
 
 class OpenAQDownloader:
     """
@@ -36,7 +35,7 @@ class OpenAQDownloader:
             location: Location,
             output_dir: Path,
             variable: str,
-            time_range=None
+            time_range: Dict[str, str] = None
     ):
         self.api = openaq.OpenAQ()
         if time_range is None:
@@ -50,9 +49,7 @@ class OpenAQDownloader:
             raise NotImplementedError(f"The variable {variable} do"
                                       f" not correspond to any known one")
 
-    def run(
-            self
-    ) -> str:
+    def run(self) -> str:
         """
         Main method to download data from OpenAQ.
         """
@@ -63,9 +60,7 @@ class OpenAQDownloader:
         self.save_data_and_metadata(data, output_path_data, output_path_metadata)
         return f"Data has been correctly downloaded in {str(output_path_data)}"
 
-    def get_closest_station_to_location(
-            self
-    ) -> pd.Series:
+    def get_closest_station_to_location(self) -> pd.Series:
         """
         Method to check which station is closer to the point of interest.
         """
@@ -93,23 +88,18 @@ class OpenAQDownloader:
         self.check_variable_in_station(station)
         return station
 
-    def get_stations_in_city(
-            self
-    ) -> pd.DataFrame:
+    def get_stations_in_city(self) -> pd.DataFrame:
         """
         Method to check whether there are stations or not in the city
         where the location of interest is located.
         """
-        stations = self.api.locations(
-            city=self.loc.city,
-            df=True
-        )
+        coord_labels = ['coordinates.latitude', 'coordinates.longitude']
+        stations = self.api.locations(city=self.loc.city, df=True)
         is_in_location = []
         for station in stations.iterrows():
-            station = station[1]
-            station_lat = round(station['coordinates.latitude'], 2)
-            station_lon = round(station['coordinates.longitude'], 2)
-            if station_lat != self.loc.latitude or station_lon != self.loc.longitude:
+            station_loc = station[1][coord_labels].values
+            loc = (self.loc.lat, self.loc.lon)
+            if np.allclose(station_loc, loc, tolerance=tol):
                 print('The OpenAQ station coordinates do not match'
                       ' with the location of interest coordinates')
                 is_in_location.append(False)
@@ -118,38 +108,29 @@ class OpenAQDownloader:
         stations['is_in_location'] = is_in_location
         return stations
 
-    def get_output_path(self, is_metadata=False):
+    def get_output_path(self, is_metadata: bool = False):
         """
         Method to get the output paths where the data and metadata are stored.
         """
         city = self.loc.city.lower()
         station_id = self.loc.location_id.lower()
         variable = self.variable
-        time_range = f"{self.time_range['start'].replace('-', '')}_{self.time_range['end'].replace('-', '')}"
-        if not is_metadata:
-            output_path = Path(
-                self.output_dir,
-                city,
-                station_id,
-                f"{variable}_{city}_{station_id}_{time_range}.csv"
-            )
-        else:
-            output_path = Path(
-                self.output_dir,
-                city,
-                station_id,
-                f"{variable}_{city}_{station_id}_{time_range}_metadata.txt"
-            )
+        time_range = '_'.join(self.time_range.values()).replace('-', '')
+        ext = '_metadata.txt' if is_metadata else '.csv'
+        output_path = Path(
+            self.output_dir,
+            city,
+            station_id,
+            f"{variable}_{city}_{station_id}_{time_range}{ext}"
+        )
         return output_path
 
-    def get_data(
-            self,
-            station
-    ) -> pd.DataFrame:
+    def get_data(self, station: str) -> pd.DataFrame:
         """
-        This methods retrieves data from the OpenAQ platform in pd.DataFrame format.
-        The specific self.time_range, given by the user, is selected afterwards. It
-        also takes out every value under 0 (which are considered as NaN).
+        This methods retrieves data from the OpenAQ platform in pd.DataFrame 
+        format. The specific self.time_range, given by the user, is selected 
+        afterwards. It also takes out every value under 0 (which are considered 
+        as NaN).
         """
         data = self.api.measurements(
             city=station['city'],
@@ -158,7 +139,8 @@ class OpenAQDownloader:
             limit=100000,
             df=True)
         data_in_time = data[
-            (data['date.utc'] > self.time_range['start']) & (data['date.utc'] <= self.time_range['end'])
+            (data['date.utc'] > self.time_range['start']) & 
+            (data['date.utc'] <= self.time_range['end'])
         ]
         # The date.utc columns is set as index in order to have always the
         # same time reference
@@ -167,10 +149,7 @@ class OpenAQDownloader:
         data_in_time = data_in_time[data_in_time['value'] >= 0]
         return data_in_time
 
-    def check_variable_in_station(
-            self,
-            station: pd.Series
-    ):
+    def check_variable_in_station(self, station: pd.Series):
         """
         This method checks whether the stations has available data for the
         variable of interest
@@ -186,30 +165,32 @@ class OpenAQDownloader:
             output_path_metadata: Path
     ):
         """
-        This function saves the data in .csv format and the metadata in .txt format
+        This function saves the data (.csv format) or the metadata (.txt format)
         """
         if not output_path_data.parent.exists():
             os.makedirs(output_path_data.parent, exist_ok=True)
         if not output_path_metadata.parent.exists():
             os.makedirs(output_path_metadata.parent, exist_ok=True)
         data['value'].to_csv(output_path_data)
-        metadata = f"For the parameter {self.variable} in units of {np.unique(data['unit'].values)}, " \
-                   f"there is a total of {len(data)} values ranging from {data.index.values[0]} " \
-                   f"to {data.index.values[-1]}. All of them with a value equal or above 0."
+        metadata = f"For the parameter {self.variable} in units of " \
+                   f"{np.unique(data['unit'].values)}, there is a total of " \
+                   f"{len(data)} values ranging from {data.index.values[0]} " \
+                   f"to {data.index.values[-1]}. All of them with a value " \
+                   f"equal or above 0."
         with open(output_path_metadata, 'w') as f:
             f.write(metadata)
             f.close()
 
 
 def get_distance_between_two_points_on_earth(
-        lat1,
-        lat2,
-        lon1,
-        lon2
+        lat1: float,
+        lat2: float,
+        lon1: float,
+        lon2: float
 ):
     """
     Function to calculate the distance between an station from OpenAQ
-    and the coordinates of interest
+    and the coordinates of interest. Use Haversine distance.
     """
     R = 6373.0
     lat1 = radians(lat1)
@@ -218,12 +199,7 @@ def get_distance_between_two_points_on_earth(
     lon2 = radians(lon2)
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = R * c
     return distance
-
-
-if __name__ == '__main__':
-    loc = Location("AE001", "Dubai", "United Arab Emirates", 25.0657, 55.17128)
-    OpenAQDownloader(loc, Path('/predictia-nas2/Data/'), 'o3').run()
