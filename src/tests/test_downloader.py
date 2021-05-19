81a5c4e39aae5f792c5ef4d1b7e8841d7fb8a7c6
santacruzm <mariosanta_cruz@hotmail.com>
2021-05-19 11:27:49 +0200

    Test relying in remote directory

--- src/tests/test_downloader.py
+++ src/tests/test_downloader.py
@@ -3,22 +3,29 @@ from src.tests.file_provider import get_remote_file
 from pathlib import Path
 
 import tempfile
+import filecmp
+
+tempdir = tempfile.mkdtemp()
 
 
-class TestDownloaderAQ:
-    def test_download_aq(self):
-        tempdir = tempfile.mkdtemp()
-        get_remote_file("dubai", tempdir)
+def test_download_aq():
+    latitude_dubai = 25.0657
+    longitude_dubai = 55.17128
+    filename = "dubai/ae001/o3_dubai_ae001_20190101_20210331"
+    data_filename = filename + ".csv"
+    metadata_filename = filename + "_metadata.csv"
+    
+    # Load the data into the temporary directory.        
+    get_remote_file(data_filename, tempdir)
+    get_remote_file(metadata_filename, tempdir)
 
-        latitude_dubai = 25.0657
-        longitude_dubai = 55.17128
-        data_path = Path('/tmp/test_download_aq/')
-        
-        loc = Location(
-            "AE001", "Dubai", "United Arab Emirates", 
-            latitude_dubai, longitude_dubai)
-        OpenAQDownloader(loc, data_path, 'o3').run() 
-
-
-if __name__ == '__main__':
-    TestDownloaderAQ().test_download_aq()
+    loc = Location(
+        "AE001", "Dubai", "United Arab Emirates", 
+        latitude_dubai, longitude_dubai)
+    OpenAQDownloader(loc, tempdir + "/downloaded", 'o3').run() 
+    
+    assert filecmp.dircmp(tempdir + "/downloaded", tempdir)
+    assert filecmp.dircmp(f"{tempdir}/downloaded/{data_filename}", 
+                            f"{tempdir}/{data_filename}")
+    assert filecmp.dircmp(f"{tempdir}/downloaded/{metadata_filename}", 
+                            f"{tempdir}/{metadata_filename}")
