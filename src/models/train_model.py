52cf5f094772b38cf0e3d633fdf4ab68d82e79fe
santacruzm <santacruzm@predictia.es>
2021-06-21 11:23:42 +0200

    model trainer update

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -9,8 +9,11 @@ from sklearn.model_selection import GridSearchCV
 
 from src.data.load.load_data import DataLoader
 from src.models.utils import read_yaml
+from src.constants import ROOT_DIR
 
 warnings.filterwarnings('ignore')
+
+logger = logging.getLogger("Model trainer")
 
 models_dict = {
     'xgboost_regressor': xg.XGBRegressor()
@@ -19,19 +22,24 @@ models_dict = {
 
 class ModelTrain:
     def __init__(
-            self,
-            config_yml: Path
+        self,
+        config_yml_filename: Path,
+        config_folder: str = ROOT_DIR / "models" / "configuration"
     ):
-        self.config = read_yaml(config_yml)
+        config = read_yaml(config_folder / config_yml_filename)
+        self.variable = config['data']['variable']
+        self.idir = config['data']['idir']
+        self.odir = config['data']['odir']
+        self.models = config['models']
 
     def run(self):
-        for i, model in enumerate(self.config['models']):
-            logging.info(f'Training and validating model {i+1} '
+        data_dict = DataLoader(self.variable, self.idir).data_load()
+        for i, model in enumerate(self.models):
+            logger.info(f'Training and validating model {i+1} '
                          f'out of {len(self.config["models"])}')
-            logging.info(f'Loading data for variable {model["variable"]}')
-            data_dict = DataLoader(model['variable'],
-                                   Path(model['data_dir'])).data_load()
-            logging.info(f'Training model with method {model["type"]}')
+            logger.info(f'Loading data for variable {model["variable"]}')
+            
+            logger.info(f'Training model with method {model["type"]}')
             gridsearch = self.train_and_validate(model, data_dict)
             model_output_path = self.get_model_output_path(gridsearch, model)
 
@@ -40,16 +48,16 @@ class ModelTrain:
         parameters = model['model_parameters'][0]
         training_methods = model['training_method'][0]
 
-        gridsearch = GridSearchCV(model_instance,
-                                parameters,
-                                cv=training_methods['cv'],
-                                scoring=training_methods['scoring'],
-                                n_jobs=training_methods['n_jobs'],
-                                verbose=training_methods['verbose'])
+        gridsearch = GridSearchCV(
+            model_instance,
+            parameters,
+            cv=training_methods['cv'],
+            scoring=training_methods['scoring'],
+            n_jobs=training_methods['n_jobs'],
+            verbose=training_methods['verbose'])
 
         print(data['train'][1].mean().values)
-        gridsearch.fit(data['train'][0],
-                     data['train'][1])
+        gridsearch.fit(data['train'][0], data['train'][1])
 
         # Print the r2 score
         print(r2_score(data['test'][1],
@@ -68,4 +76,4 @@ class ModelTrain:
 
 
 if __name__ == '__main__':
-    ModelTrain(Path('../../models/configuration/model_config.yml')).run()
+    ModelTrain("model_config.yml").run()
