e2399cab0d4cc9e991db5e6b53daad04df48ae1b
JPXKQX <mariosanta_cruz@hotmail.com>
2021-06-21 10:17:53 +0000

    Undo change in ROOT_DIR

--- src/constants.py
+++ src/constants.py
@@ -7,6 +7,6 @@ str2agg = {
 }
 
 
-ROOT_DIR = Path(__file__).parent.parent
+ROOT_DIR = Path(os.path.dirname(os.path.abspath("setup.py")))
 
 log_fmt = '%(asctime)s | %(name)s | %(levelname)s | %(message)s'
