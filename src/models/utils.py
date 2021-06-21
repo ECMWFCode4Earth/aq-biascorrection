618e479f26201cef29193c663508a0508ee6c26d
santacruzm <santacruzm@predictia.es>
2021-06-21 11:23:05 +0200

    More logs

--- src/models/utils.py
+++ src/models/utils.py
@@ -3,11 +3,16 @@ import logging
 import yaml
 
 
+logger = logging.getLogger("Model utilities")
+
+
 def read_yaml(yaml_path):
+    logger.debug(f"Loading YAML file at {yaml_path}")
     with open(yaml_path, 'r') as stream:
         try:
             json = yaml.safe_load(stream)
+            logger.debug(f"YAML file at {yaml_path} loaded succesfully.")
             return json
         except yaml.YAMLError as exc:
-            logging.error(exc)
+            logger.error(exc)
             raise exc
