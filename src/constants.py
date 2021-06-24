49bf6653e427fbc0a49be9f428d407a55b64e2d4
JPXKQX <mariosanta_cruz@hotmail.com>
2021-06-24 07:44:25 +0000

    Moved to constants

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,23 @@
 import os
 from pathlib import Path
 
+
 str2agg = {
     'daily': 'D',
     'monthly': 'M'
+}
+
+
+var2longstr = {
+    'no2': 'Nitrogen dioxide',
+    'o3': 'Ozone',
+    'pm25': 'Particulate matter (PM2.5)'
+}
+
+
+units2str = {
+    "µg/m³": "microgram / m^3",
+    "ppm": "ppm"
 }
 
 
