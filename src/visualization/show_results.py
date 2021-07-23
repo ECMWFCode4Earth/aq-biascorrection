99f12810982322593dc1b3a997ad902fefbdb72f
JPXKQX <mariosanta_cruz@hotmail.com>
2021-07-23 08:05:20 +0000

    Showing results

--- src/visualization/show_results.py
+++ src/visualization/show_results.py
@@ -3,8 +3,15 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import pandas as pd
 
+from ipywidgets import interactive, widgets
 from src.constants import ROOT_DIR
+from matplotlib.dates import DateFormatter
+from typing import List
 from pydantic.dataclasses import dataclass
+
+
+df_stations = pd.read_csv(ROOT_DIR / "data" / "external" / "stations.csv", index_col=0)
+date_form = DateFormatter("%-d %b %y")
 
 
 @dataclass
@@ -31,22 +38,48 @@ class ResultsPlotter:
         idir = ROOT_DIR / "data" / "processed"
         data_file = list(idir.rglob(f"data_{self.varname}_{station}.csv"))[0]
         data = pd.read_csv(data_file, index_col=0)
-        data['index'] = pd.to_datetime(data.index)
+        data['index'] = pd.to_datetime(data['index'])
         return data.set_index('index')
 
-    def run(self, station_code: str):
+    def run(self, station_code: str, xlim: tuple = None):
         df = self.load_data(station_code)
         data = self.load_obs_preds(station_code)
 
+        plt.figure(figsize=(17, 9))
         data[f"{self.varname}_forecast"].plot(color='b', label='CAMS forecast')
         data[f"{self.varname}_observed"].plot(color='k', label='Observation')
+
+        is_first = True
         for init_time, values in df.iterrows():
-            # TODO: These are corrections so must be joined with predicitons.
             indices = pd.date_range(start=init_time, periods=len(values), freq='H')
-            plt.plot(indices, values.values, color='orange')
+            (data.loc[indices, "pm25_forecast"] - values.values).plot(
+                color='orange',
+                label="CAMS + Correction" if is_first else '_nolegend_'
+            )
+            is_first = None
+        plt.legend()
+        plt.ylabel(self.varname + r' ($\mu g / m^2$)')
+        plt.xlabel("Date", fontsize='xx-large')
+        ax = plt.gca()
+        ax.xaxis.set_major_formatter(date_form)
+        if xlim is not None:
+            plt.xlim(xlim)
         plt.show()
 
 
+# Methods for implementation of Jupyter Tool
+def get_all_locations() -> List[str]:
+    return list(df_stations.city.unique())
+
+
+def get_id_location(city: str) -> str:
+    return df_stations.loc[df_stations.city == city, "id"].values[0]
+
+
+def interactive_viz(varname: str, station: str, date_range: tuple):
+    plotter = ResultsPlotter("InceptionTime_ensemble", varname)
+    plotter.run(get_id_location(station), date_range)
+
 
 if __name__ == '__main__':
-    ResultsPlotter("InceptionTime_ensemble", "pm25").run("GB002")
+    ResultsPlotter('InceptionTime_ensemble', 'pm25').run("GB002")
